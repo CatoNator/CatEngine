7caf9f89a644f58b338115fddc44e3eb914f49cc
CatoNator <jesse.j.koivisto@hotmail.com>
2018-09-18 13:26:53 +0300

    implemented tank controls

--- CatEngine/GameObjects/CPlayer.cs
+++ CatEngine/GameObjects/CPlayer.cs
@@ -11,15 +11,14 @@ namespace CatEngine
 {
     public class CPlayer : CGameObject
     {
-        private bool bLanded = false;
+        public float fAimDirection = 0.0f;
 
-        public bool bFlip;
+        private int iSpeed = 0;
         
         public override void InstanceSpawn()
         {
-            vCollisionOrigin = new Vector2(17, 17);
-            rCollisionRectangle = new Rectangle(0, 0, 34, 35);
-            bFlip = false;
+            vCollisionOrigin = new Vector2(16, 16);
+            rCollisionRectangle = new Rectangle(0, 0, 32, 32);
         }
         
         public override void Update()
@@ -29,42 +28,59 @@ namespace CatEngine
             //floor collision
             CGameObject collision = CollisionRectangle(new Rectangle(rCollisionRectangle.X, rCollisionRectangle.Y + rCollisionRectangle.Height, rCollisionRectangle.Width, 1), typeof(CWall), true);
 
-            if (collision != null && fVerSpeed > 0)
+            /*if (collision != null && fVerSpeed > 0)
             {
                 bLanded = true;
                 y = collision.y - vCollisionOrigin.Y;
                 fVerSpeed = 0;
             }
             else
-                bLanded = false;
+                bLanded = false;*/
 
             //input
             KeyboardState keyboardState = Keyboard.GetState();
 
+            //rename this var
             int maxSpeed = 2;
 
-            //moving
-            if (keyboardState.IsKeyDown(Keys.Left) &&
-                CollisionRectangle(new Rectangle(rCollisionRectangle.X - maxSpeed + 1, rCollisionRectangle.Y, maxSpeed + 1, rCollisionRectangle.Height - 3), typeof(CWall), true) == null)
-            {
-                fHorSpeed = -maxSpeed;
-                bFlip = true;
-            }  
-            else if (keyboardState.IsKeyDown(Keys.Right) &&
-                CollisionRectangle(new Rectangle(rCollisionRectangle.X + rCollisionRectangle.Width, rCollisionRectangle.Y, maxSpeed + 1, rCollisionRectangle.Height - 3), typeof(CWall), true) == null)
-            {
-                fHorSpeed = maxSpeed;
-                bFlip = false;
-            }
+            //turning
+            if (keyboardState.IsKeyDown(Keys.Left))
+                fAimDirection += 2.0f;
+            else if (keyboardState.IsKeyDown(Keys.Right))
+                fAimDirection -= 2.0f;
+
+            if (fAimDirection >= 360)
+                fAimDirection -= 360;
+            else if (fAimDirection <= 0)
+                fAimDirection += 360;
+
+            //moving forward/backward
+            if (keyboardState.IsKeyDown(Keys.Up))
+                iSpeed = maxSpeed;
+            else if (keyboardState.IsKeyDown(Keys.Down))
+                iSpeed = -maxSpeed;
             else
+                iSpeed = 0;
+
+            //strafing??
+
+            fHorSpeed = (float)distDirX((float)iSpeed, degToRad(fAimDirection));
+            fVerSpeed = (float)distDirY((float)iSpeed, degToRad(fAimDirection));
+
+            //note! current collision model only supports recantular collisions, no pixel perfect shapes
+            //collision always gets stuck, needs adjusting
+
+            int collisionSafeZone = 4;
+
+            //collisions to the left and right
+            if ((CollisionRectangle(new Rectangle(rCollisionRectangle.X - collisionSafeZone, rCollisionRectangle.Y, collisionSafeZone, rCollisionRectangle.Height), typeof(CWall), true) != null && fHorSpeed < 0) || 
+              (CollisionRectangle(new Rectangle(rCollisionRectangle.X + rCollisionRectangle.Width, rCollisionRectangle.Y, collisionSafeZone, rCollisionRectangle.Height), typeof(CWall), true) != null && fHorSpeed > 0))
                 fHorSpeed = 0;
 
-            //jumping
-            if (keyboardState.IsKeyDown(Keys.Up) && bLanded)
-                fVerSpeed = -6;
-
-            if (!bLanded)
-                fVerSpeed += 0.25f;
+            //collisions above and below
+            if ((CollisionRectangle(new Rectangle(rCollisionRectangle.X, rCollisionRectangle.Y - collisionSafeZone, rCollisionRectangle.Width, collisionSafeZone), typeof(CWall), true) != null && fVerSpeed < 0)||
+                (CollisionRectangle(new Rectangle(rCollisionRectangle.X, rCollisionRectangle.Y + rCollisionRectangle.Height, rCollisionRectangle.Width, collisionSafeZone), typeof(CWall), true) != null && fVerSpeed > 0))
+                fVerSpeed = 0;
 
             x += fHorSpeed;
             y += fVerSpeed;
@@ -74,7 +90,7 @@ namespace CatEngine
         {
             //CSprite.Instance.DrawRect(rCollisionRectangle, Color.Green);
 
-            CSprite.Instance.Render("sprTest", x, y, 0, bFlip, 0.0f, 1.0f, Color.White);
+            CSprite.Instance.Render("sprTest", x, y, 0, false, -degToRad(fAimDirection), 1.0f, Color.White);
         }
     }
 }
